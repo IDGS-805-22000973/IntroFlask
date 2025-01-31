360cd54ba8a8742184f3fa8ca8ec8d88e7ac8fe6
IDGS-805-22000973 <cerveraburrolaj@gmail.com>
2025-01-30 18:58:51 -0600

    Segundo commit intro Flask

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app=Flask(__name__)
 #Mandar a llamar las paginas que tenemos
 
 @app.route("/")
 def index():
-    return "Hello, World , hola"
+    titulo="IDGS801"
+    lista=["Pedro", "Juan", "Mario"]
+    return render_template("index.html",titulo=titulo,lista=lista)
 
+@app.route("/ejemplo1")
+def ejemplo1():
+    return render_template("ejemplo1.html")
+
+@app.route("/ejemplo2")
+def ejemplo2():
+    return render_template("ejemplo2.html")
 
 @app.route("/Hola")
 def hola():
-    return "Hola, Mundo"
+    return "<h1>Hola, Mundo</h1>"
 
+@app.route("/user/<string:user>")
+def user(user):
+    return f"Hola, {user}!"
+
+@app.route("/numero<int:n>")
+def numero(n):
+    return f"El numero es : {n}!"
+
+@app.route("/user/<int:id>/<string:username>")
+def username(id, username):
+    return f"El ususario es : {username}! con id: {id}!"
+
+@app.route("/suma/<float:n1>/<float:n2>")
+def suma(n1,n2):
+    return f"La suma es: {n1,n2}!"
+
+@app.route("/default/")
+@app.route("/default/<string:tem>")
+def func1(tem='Juan'):
+    return f"Hola, {tem}!"
+
+
+@app.route("/form1")
+def form1():
+    return '''
+    <form>
+    <label for=nombre> nombre: </label>
+    </form>
+    '''
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
